3c0c3b4d99a03ef5d46bffc97ddff1304cd6c9cb
Andrew Milich <milichab@gmail.com>
2025-11-29 14:33:52 -0500

    scaling

--- src/lib/renderConfig.ts
+++ src/lib/renderConfig.ts
@@ -407,7 +407,8 @@ const SPRITE_PACK_SPRITES4: SpritePack = {
   },
   parksScales: {
     baseball_stadium: 0.81,  // 10% smaller than 0.90
-    baseball_field_small: 0.95,  // scaled down 5%
+    baseball_field_small: 0.855,  // scaled down 10% from 0.95
+    football_field: 0.9,  // scaled down 10%
     swimming_pool: 0.90,  // scaled down 10% total (5% more from 0.95)
     soccer_field_small: 0.95,  // scaled down 5%
     go_kart_track: 0.92,  // scaled down 8%
